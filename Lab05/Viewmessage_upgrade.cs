cb88dfad57d0fc842bdd98a18c1877b9eecc5803
ToanLe <whiteboyhihihi@gmail.com>
2023-06-01 13:58:45 +0700

    lab5_lan2

--- Lab05/Viewmessage_upgrade.cs
+++ Lab05/Viewmessage_upgrade.cs
@@ -8,20 +8,30 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Web;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Tab;
 
 namespace Lab05
 {
     public partial class Viewmessage_upgrade : Form
     {
-        public Viewmessage_upgrade(string mess, string from, string to, string sub, string acc, string pass)
+        public Viewmessage_upgrade(string html, string from, string to, string sub, string acc, string pass)
         {
             InitializeComponent();
-            richTextBox1.Text = mess;
             text_from.Text = from;
             textto.Text = to;
             this.Text = sub;
             frome = to; toe = from;
             pas = pass; ac = acc;
+            InitBrowser(html);
+        }
+        private async Task initizated()
+        {
+            await webView21.EnsureCoreWebView2Async(null);
+        }
+        public async void InitBrowser(string s)
+        {
+            await initizated();
+            webView21.CoreWebView2.NavigateToString(s);
         }
         string frome, toe, ac, pas;
         private void button1_Click(object sender, EventArgs e)
